5a7fb7cf2fd0e93b04ad94299e2502a185699391
源稚生 <yzhisheng94666@gmail.com>
2025-05-25 07:22:03 +0800

    service更新

---
+++ takeaway-server/src/main/java/com/sakura/takeaway/service/AddressBookService.java
@@ -0,0 +1,22 @@
+package com.sakura.takeaway.service;
+
+
+import com.sakura.takeaway.entity.AddressBook;
+
+import java.util.List;
+
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
---
+++ takeaway-server/src/main/java/com/sakura/takeaway/service/ShoppingCartService.java
@@ -0,0 +1,33 @@
+package com.sakura.takeaway.service;
+
+
+import com.sakura.takeaway.dto.ShoppingCartDTO;
+import com.sakura.takeaway.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
+
+    /**
+     * 清空购物车
+     */
+    void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
