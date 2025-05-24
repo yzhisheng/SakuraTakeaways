4f1613355d2381a14abe090f778e9b0791328d78
源稚生 <yzhisheng94666@gmail.com>
2025-05-25 07:01:07 +0800

    查询分类、获取店铺的营业状态、根据分类id查询套餐、根据套餐id查询包含的菜品列表

---
+++ takeaway-server/src/main/java/com/sakura/takeaway/service/DishService.java
@@ -0,0 +1,73 @@
+package com.sakura.takeaway.service;
+
+
+import com.sakura.takeaway.dto.DishDTO;
+import com.sakura.takeaway.dto.DishPageQueryDTO;
+import com.sakura.takeaway.entity.Dish;
+import com.sakura.takeaway.result.PageResult;
+import com.sakura.takeaway.vo.DishVO;
+
+import java.util.List;
+
+public interface DishService {
+
+    /**
+     * 新增菜品和对应的口味
+     *
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     *
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     *
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 根据id修改菜品基本信息和对应的口味信息
+     *
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品起售停售
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据分类id查询菜品
+     *
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
+}
---
+++ takeaway-server/src/main/java/com/sakura/takeaway/service/SetmealService.java
@@ -0,0 +1,73 @@
+package com.sakura.takeaway.service;
+
+
+import com.sakura.takeaway.dto.SetmealDTO;
+import com.sakura.takeaway.dto.SetmealPageQueryDTO;
+import com.sakura.takeaway.entity.Setmeal;
+import com.sakura.takeaway.result.PageResult;
+import com.sakura.takeaway.vo.DishItemVO;
+import com.sakura.takeaway.vo.SetmealVO;
+
+import java.util.List;
+
+public interface SetmealService {
+
+    /**
+     * 新增套餐，同时需要保存套餐和菜品的关联关系
+     *
+     * @param setmealDTO
+     */
+    void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * 分页查询
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 批量删除套餐
+     *
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询套餐和关联的菜品数据
+     *
+     * @param id
+     * @return
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 修改套餐
+     *
+     * @param setmealDTO
+     */
+    void update(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐起售、停售
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    List<DishItemVO> getDishItemById(Long id);
+}
